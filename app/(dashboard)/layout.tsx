dc2b9b9656e1e9e1f54be6a90baad2ee868cdaa8
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-09-27 16:41:40 +0100

    logo created

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
-    children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
@@ -7,7 +9,12 @@ export default function DashboardLayout({
       <html lang="en">
         <div className="h-screen flex">
             {/* LEFT-SIDE-SIDEBAR  */}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">L</div>    
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+              <Image src="/logo.png" alt="logo" width={32} height={32} />
+              <span className="hidden lg:block">CoronaSchool</span>
+            </Link>
+          </div>    
             {/* RIGHT-SIDE-CONTENT */}
             <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">R</div>
         </div>
