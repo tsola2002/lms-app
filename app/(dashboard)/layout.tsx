7fecd0a3ea2d6626d00219e12b246695fc33372c
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-09-27 16:05:18 +0100

    full folder resturucting done

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <div className="h-screen flex">
+            {/* LEFT-SIDE-SIDEBAR  */}
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">L</div>    
+            {/* RIGHT-SIDE-CONTENT */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">R</div>
+        </div>
+      </html>
+    );
+  }
