2ecd068ab38c25db1dd3d6804b11a3642a50d435
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-09-27 21:37:44 +0100

    Added different charts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lamaSky: "#C3EBFA",
+        lamaSkyLight: "#EDF9FD",
+        lamaPurple: "#CFCEFF",
+        lamaPurpleLight: "#F1F0FF",
+        lamaYellow: "#FAE27C",
+        lamaYellowLight: "#FEFCE8",
       },
     },
   },
