c1c1c0c5d808c6c2f1635c4a4a7f02ba1e85bd44
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-09-27 15:16:35 +0100

    layout file added to dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      {children}
+      test{children}
       </body>
     </html>
   );
