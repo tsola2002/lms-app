978902750d72aed72519f26a58be4dfd60f7c4d0
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-09-30 15:18:06 +0100

    Added Prisma orm to the frontend

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
@@ -18,6 +19,10 @@ const AdminPage = () => {
           {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
           </div>
         </div>
       </div>
