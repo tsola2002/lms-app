bb1ea4e87ad64617857074723dafbf988452e243
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-10-07 12:39:49 +0100

    Improved Teacher Page Design

--- app/(dashboard)/list/teachers/page.tsx
+++ app/(dashboard)/list/teachers/page.tsx
@@ -3,91 +3,100 @@ import Image from "next/image";
 // import Link from "next/link";
 import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
+import Table from "@/components/Table";
+import Link from "next/link";
+import { role, teachersData } from "@/lib/data";
 
-// type Teacher = {
-//     id: number;
-//     teacherId: string;
-//     name: string;
-//     email?: string;
-//     photo: string;
-//     phone: string;
-//     subjects: string[];
-//     classes: string[];
-//     address: string;
-// };
+type Teacher = {
+    id: number;
+    teacherId: string;
+    name: string;
+    email?: string;
+    photo: string;
+    phone: string;
+    subjects: string[];
+    classes: string[];
+    address: string;
+};
   
-// const columns = [
-//     {
-//       header: "Info",
-//       accessor: "info",
-//     },
-//     {
-//       header: "Teacher ID",
-//       accessor: "teacherId",
-//       className: "hidden md:table-cell",
-//     },
-//     {
-//       header: "Subjects",
-//       accessor: "subjects",
-//       className: "hidden md:table-cell",
-//     },
-//     {
-//       header: "Classes",
-//       accessor: "classes",
-//       className: "hidden md:table-cell",
-//     },
-//     {
-//       header: "Phone",
-//       accessor: "phone",
-//       className: "hidden lg:table-cell",
-//     },
-//     {
-//       header: "Address",
-//       accessor: "address",
-//       className: "hidden lg:table-cell",
-//     },
-//     {
-//       header: "Actions",
-//       accessor: "action",
-//     },
-//   ];
+const columns = [
+    {
+      header: "Info",
+      accessor: "info",
+    },
+    {
+      header: "Teacher ID",
+      accessor: "teacherId",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Subjects",
+      accessor: "subjects",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Classes",
+      accessor: "classes",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Phone",
+      accessor: "phone",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Address",
+      accessor: "address",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Actions",
+      accessor: "action",
+    },
+  ];
 
 const TeacherListPage = () => {
 
-    // const renderRow = (item: Teacher) => (
-    //     <tr
-    //       key={item.id}
-    //       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    //     >
-    //       <td className="flex items-center gap-4 p-4">
-    //         <Image
-    //           src={item.photo}
-    //           alt=""
-    //           width={40}
-    //           height={40}
-    //           className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-    //         />
-    //         <div className="flex flex-col">
-    //           <h3 className="font-semibold">{item.name}</h3>
-    //           <p className="text-xs text-gray-500">{item?.email}</p>
-    //         </div>
-    //       </td>
-    //       <td className="hidden md:table-cell">{item.teacherId}</td>
-    //       <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-    //       <td className="hidden md:table-cell">{item.classes.join(",")}</td>
-    //       <td className="hidden md:table-cell">{item.phone}</td>
-    //       <td className="hidden md:table-cell">{item.address}</td>
-    //       <td>
-    //         <div className="flex items-center gap-2">
-    //           <Link href={`/list/teachers/${item.id}`}>
-    //             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-    //               <Image src="/view.png" alt="" width={16} height={16} />
-    //             </button>
-    //           </Link>      
-    //         </div>
-    //       </td>
-    //     </tr>
-    //   );
+    const renderRow = (item: Teacher) => (
+        <tr
+          key={item.id}
+          className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+        >
+          <td className="flex items-center gap-4 p-4">
+            <Image
+              src={item.photo}
+              alt=""
+              width={40}
+              height={40}
+              className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+            />
+            <div className="flex flex-col">
+              <h3 className="font-semibold">{item.name}</h3>
+              <p className="text-xs text-gray-500">{item?.email}</p>
+            </div>
+          </td>
+          <td className="hidden md:table-cell">{item.teacherId}</td>
+          <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+          <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+          <td className="hidden md:table-cell">{item.phone}</td>
+          <td className="hidden md:table-cell">{item.address}</td>
+          <td>
+            <div className="flex items-center gap-2">
+              <Link href={`/list/teachers/${item.id}`}>
+                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+                  <Image src="/view.png" alt="" width={16} height={16} />
+                </button>
+              </Link>
+              {/* {role === "admin" && (
+                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+                  <Image src="/delete.png" alt="" width={16} height={16} />
+                </button>
+                // <FormModal table="teacher" type="delete" id={item.id}/>
+              )} */}
+            </div>
+          </td>
+        </tr>
+      );
 
 
       return (
@@ -109,7 +118,8 @@ const TeacherListPage = () => {
             </div>
           </div>
           {/* LIST */}
-          {/* <Table columns={columns} renderRow={renderRow} data={teachersData} /> */}
+          <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+          
           {/* PAGINATION */}
           <Pagination />
         </div>
